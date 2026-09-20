0f8dcdae0a7b2e2058b9aa77a0423ca66b26ff1f
Cj Obi <cj.obi@ucalgary.ca>
2026-09-20 07:47:06 -0600

    Fundamental function for Fibonnaci sequence

---
+++ Assignment1.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+// Function to calculate the nth Fibonacci number
+int fibonacci(int n) {
+    if (n == 0) {
+        return 0;
+    } else if (n == 1) {
+        return 1;
+    } else {
+        return fibonacci(n - 1) + fibonacci(n - 2);
+    }
+}
+
+int main() {
+    int num;
+    printf("Enter a number: ");
+    scanf("%d", &num);  
+
+    printf("Fibonacci of %d is: %d\n", num, fibonacci(num));
+    return 0;
+}
